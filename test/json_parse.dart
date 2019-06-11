c0e8efccd7f4dcf624742e1b2c08f1e5f0c2eacb
caicheng <caicheng@hooook.com>
2019-06-11 21:40:01 +0800

    更新

--- test/json_parse.dart
+++ test/json_parse.dart
@@ -12,10 +12,10 @@ void main() {
       //如果expect中两个参数的结果是相同的的就算测试通过
       //如果不相同就错误
 
-//      expect(data1.url,
-//          'http://www.getdropbox.com/u/2/screencast.html'); //结果All Tests Passed
+      expect(data1.url,
+          'http://www.getdropbox.com/u/2/screencast.html'); //结果All Tests Passed
 
-      expect(data1.url, '错误的数据'); //报错
+//      expect(data1.url, '错误的数据'); //报错
     });
   });
 }
